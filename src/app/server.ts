1590bdcaa4d06d2aaec55dab95f07765578e579f
José Rodolpho <j.rodolpho29@hotmail.com>
2022-11-09 03:19:17 -0300

    feat: setup new project and database

---
+++ src/app/server.ts
@@ -0,0 +1,8 @@
+import app from "./index.js"
+import dotenv from "dotenv"
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
